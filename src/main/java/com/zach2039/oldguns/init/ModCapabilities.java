8dff2f6c6df1eca73fcafc84b9bfd70174de39bc
zach2039 <sumyunguy2007@gmail.com>
2022-01-19 11:01:02 -0600

    Update capability register for 1.18.1

--- src/main/java/com/zach2039/oldguns/init/ModCapabilities.java
+++ src/main/java/com/zach2039/oldguns/init/ModCapabilities.java
@@ -3,15 +3,15 @@ package com.zach2039.oldguns.init;
 import com.zach2039.oldguns.OldGuns;
 import com.zach2039.oldguns.capability.firearmempty.FirearmEmptyCapability;
 
+import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod.EventBusSubscriber(modid = OldGuns.MODID, bus = Bus.MOD)
 public class ModCapabilities {
 	@SubscribeEvent
-	public static void registerCapabilities(final FMLCommonSetupEvent event) {
-		FirearmEmptyCapability.register();
+	public static void registerCapabilities(final RegisterCapabilitiesEvent event) {
+		FirearmEmptyCapability.register(null);
 	}
 }
