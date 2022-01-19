a27ba1c758e57bf6b9c2b00c5bf95b8f5d0f6a00
zach2039 <sumyunguy2007@gmail.com>
2022-01-19 10:59:42 -0600

    Add capabilities registry fix from 1.16.5 branch

---
+++ src/main/java/com/zach2039/oldguns/init/ModCapabilities.java
@@ -0,0 +1,17 @@
+package com.zach2039.oldguns.init;
+
+import com.zach2039.oldguns.OldGuns;
+import com.zach2039.oldguns.capability.firearmempty.FirearmEmptyCapability;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+@Mod.EventBusSubscriber(modid = OldGuns.MODID, bus = Bus.MOD)
+public class ModCapabilities {
+	@SubscribeEvent
+	public static void registerCapabilities(final FMLCommonSetupEvent event) {
+		FirearmEmptyCapability.register();
+	}
+}
