6dc0dd2387d81378da83b7fd9e19b3c9e383fcbe
zach2039 <sumyunguy2007@gmail.com>
2022-02-05 16:13:59 -0600

    Closes #14
    Fix mortar and pestle recipe consuming item in vanilla crafting bench

--- src/main/java/com/zach2039/oldguns/world/item/crafting/recipe/ShapelessVanillaMortarAndPestleRecipe.java
+++ src/main/java/com/zach2039/oldguns/world/item/crafting/recipe/ShapelessVanillaMortarAndPestleRecipe.java
@@ -1,22 +1,104 @@
 package com.zach2039.oldguns.world.item.crafting.recipe;
 
+import com.google.gson.JsonObject;
 import com.zach2039.oldguns.init.ModCrafting;
-import com.zach2039.oldguns.world.item.crafting.DamageableToolRecipe;
+import com.zach2039.oldguns.world.item.crafting.util.ModRecipeUtil;
+import com.zach2039.oldguns.world.item.tools.MortarAndPestleItem;
 
 import net.minecraft.core.NonNullList;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.ShapelessRecipe;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.event.ForgeEventFactory;
+import net.minecraftforge.registries.ForgeRegistryEntry;
 
-public class ShapelessVanillaMortarAndPestleRecipe extends ShapelessRecipe implements DamageableToolRecipe {
+public class ShapelessVanillaMortarAndPestleRecipe extends ShapelessRecipe {
 	
-	public ShapelessVanillaMortarAndPestleRecipe(ResourceLocation p_44246_, String p_44247_, ItemStack p_44248_,
-			NonNullList<Ingredient> p_44249_) {
-		super(p_44246_, p_44247_, p_44248_, p_44249_);
+	public ShapelessVanillaMortarAndPestleRecipe(ResourceLocation id, String group, ItemStack result,
+			NonNullList<Ingredient> ingredients) {
+		super(id, group, result, ingredients);
 	}
 
+	@Override
+	public NonNullList<ItemStack> getRemainingItems(final CraftingContainer inv) {
+		final NonNullList<ItemStack> remainingItems = NonNullList.withSize(inv.getContainerSize(), ItemStack.EMPTY);
+
+		for (int i = 0; i < remainingItems.size(); ++i) {
+			final ItemStack itemstack = inv.getItem(i);
+
+			if (!itemstack.isEmpty() && (itemstack.getItem() instanceof MortarAndPestleItem)) {
+				remainingItems.set(i, damageItem(itemstack.copy()));
+			} else {
+				remainingItems.set(i, ForgeHooks.getContainerItem(itemstack));
+			}
+		}
+
+		return remainingItems;
+	}
+
+	private ItemStack damageItem(final ItemStack stack) {
+		final Player craftingPlayer = ForgeHooks.getCraftingPlayer();
+
+		Level level = craftingPlayer.getCommandSenderWorld();
+		if (stack.hurt(1, level.random, craftingPlayer instanceof ServerPlayer ? (ServerPlayer) craftingPlayer : null)) {
+			ForgeEventFactory.onPlayerDestroyItem(craftingPlayer, stack, null);
+			return ItemStack.EMPTY;
+		}
+
+		return stack;
+	}
+	
+	public static class Serializer extends ForgeRegistryEntry<RecipeSerializer<?>> implements RecipeSerializer<ShapelessVanillaMortarAndPestleRecipe> {
+
+		@Override
+		public ShapelessVanillaMortarAndPestleRecipe fromJson(final ResourceLocation recipeID, final JsonObject json) {
+			final String group = GsonHelper.getAsString(json, "group", "");
+			final NonNullList<Ingredient> ingredients = ModRecipeUtil.parseShapeless(json);
+			final ItemStack result = CraftingHelper.getItemStack(GsonHelper.getAsJsonObject(json, "result"), true);
+
+			ShapelessVanillaMortarAndPestleRecipe recipeFromJson = new ShapelessVanillaMortarAndPestleRecipe(recipeID, group, result, ingredients);
+
+			return recipeFromJson;
+		}
+
+		@Override
+		public ShapelessVanillaMortarAndPestleRecipe fromNetwork(final ResourceLocation recipeID, final FriendlyByteBuf buffer) {
+			final String group = buffer.readUtf(Short.MAX_VALUE);
+			final int numIngredients = buffer.readVarInt();
+			final NonNullList<Ingredient> ingredients = NonNullList.withSize(numIngredients, Ingredient.EMPTY);
+
+			for (int j = 0; j < ingredients.size(); ++j) {
+				ingredients.set(j, Ingredient.fromNetwork(buffer));
+			}
+
+			final ItemStack result = buffer.readItem();
+
+			return new ShapelessVanillaMortarAndPestleRecipe(recipeID, group, result, ingredients);
+		}
+
+		@Override
+		public void toNetwork(final FriendlyByteBuf buffer, final ShapelessVanillaMortarAndPestleRecipe recipe) {
+			buffer.writeUtf(recipe.getGroup());
+			buffer.writeVarInt(recipe.getIngredients().size());
+
+			for (final Ingredient ingredient : recipe.getIngredients()) {
+				ingredient.toNetwork(buffer);
+			}
+
+			buffer.writeItem(recipe.getResultItem());
+		}
+	}
+	
 	@Override
 	public RecipeSerializer<?> getSerializer() {
 		return ModCrafting.Recipes.MORTAR_AND_PESTLE_SHAPELESS.get();
