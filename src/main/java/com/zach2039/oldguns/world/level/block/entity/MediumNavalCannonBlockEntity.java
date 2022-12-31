cb587f23fe20d6e6463601ea3d1f9b319b9c38c4
zach2039 <sumyunguy2007@gmail.com>
2022-12-31 13:31:28 -0600

    Apply fix for client crash to naval cannon entity as well

--- src/main/java/com/zach2039/oldguns/world/level/block/entity/MediumNavalCannonBlockEntity.java
+++ src/main/java/com/zach2039/oldguns/world/level/block/entity/MediumNavalCannonBlockEntity.java
@@ -34,7 +34,7 @@ public class MediumNavalCannonBlockEntity extends StationaryCannonBlockEntity {
 	@Override
 	public void doFiringEffect(Level level, Player player, double posX, double posY, double posZ) {
 		
-		OldGuns.NETWORK.send(PacketDistributor.TRACKING_ENTITY.with(() -> player), 
+		OldGuns.NETWORK.send(PacketDistributor.TRACKING_ENTITY_AND_SELF.with(() -> player), 
 				new ArtilleryEffectMessage((LivingEntity)player, ArtilleryEffect.CANNON_SHOT, 
 						posX, posY + getShotHeight(), posZ,
 						getShotPitch(), getShotYaw(), 0)
